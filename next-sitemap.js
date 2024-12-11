74d6e8093aa5d0571ecc1f73f31660bf379b21e0
Wambui Ndungu <wambuin18@gmail.com>
2024-12-11 13:52:08 +0300

    SEO Config

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://africanplains.org',
+    generateRobotsTxt: true,  // Optionally generates a robots.txt file
+    sitemapSize: 7000,
+  }
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="African Plains Conservation of Trust is a non-profit organization that works towards sustainable conservation of select habitats and ecosystems in Kenya as well as development of neighbouring communities"
+        />
+        <meta name="keywords" content="conservation, Kenya, sustainable habitats, ecosystems, non-profit, African ecosystems, community development" />
+        <meta name="author" content="African Plains Conservation Trust" />
+        
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="African Plains Conservation Trust" />
+        <meta
+          property="og:description"
+          content="Supporting the sustainable conservation of ecosystems and the development of surrounding communities in Kenya."
+        />
+        <meta property="og:image" content="/hero-img.png" />
+        <meta property="og:url" content="https://africanplains.org" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="African Plains Conservation Trust" />
+        <meta
+          name="twitter:description"
+          content="Supporting the sustainable conservation of ecosystems and the development of surrounding communities in Kenya."
+        />
+        <meta name="twitter:image" content="/hero-img.png" />
+
+        {/* Preloading hero image */}
         <link rel="preload" href="/hero-img.png" as="image" type="image/png" />
       </Head>
       <body>
